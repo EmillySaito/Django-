91b051a28dcef4b78e882699c98a4d1e6b5e4a14
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-25 20:51:28 -0300

    Página outra

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app.views import index
+from app.views import index, outra
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('outra', outra, name='outra'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>Página Inicial</h1>')
+def outra(request):
+    return HttpResponse('<h1>Outra Página TILT</h1>')
+
 # Create your views here.
